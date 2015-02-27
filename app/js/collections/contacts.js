623aa5b483b146bc449cdfedfd14004be2f6df34
Sai Baba Satchitanand <sai.b.satchitanand@gmail.com>
2015-02-27 21:08:12 +0530

    Added search functionality and changed less as per structure.

--- app/js/collections/contacts.js
+++ app/js/collections/contacts.js
@@ -2,13 +2,23 @@
   'use strict';
   define([
     'backbone',
+    'underscore',
     'models/contact',
     'localstorage'
   ],
-  function(Backbone,Contact){
+  function(Backbone,_,Contact){
     var Contacts = Backbone.Collection.extend({
       model: Contact,
-      localStorage: new Backbone.LocalStorage("Contacts")
+      localStorage: new Backbone.LocalStorage("Contacts"),
+      search: function(text) {
+        if(text === "") return [];
+        var pattern = new RegExp(text,"i");
+        return this.filter(function(contact) {
+          return _.find(contact.toJSON(),function(value){
+            return pattern.test(value);
+          });
+        });
+      }
     });
 
     return Contacts;
